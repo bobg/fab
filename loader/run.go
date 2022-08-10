f04761dd29500abd960de998443acbacd029950a
bobg <bobg@emphatic.com>
2022-08-10 19:42:52 -0400

    Driver writes errors as a JSON list of strings.

--- loader/run.go
+++ loader/run.go
@@ -2,12 +2,14 @@ package loader
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 
 	"github.com/pkg/errors"
+	"go.uber.org/multierr"
 )
 
 func Run(ctx context.Context, pkgdir string, args ...string) error {
@@ -15,13 +17,50 @@ func Run(ctx context.Context, pkgdir string, args ...string) error {
 	if err != nil {
 		return errors.Wrap(err, "getting working directory")
 	}
+	tmpfile, err := os.CreateTemp("", "fab")
+	if err != nil {
+		return errors.Wrap(err, "creating tempfile")
+	}
+	err = tmpfile.Close()
+	if err != nil {
+		return errors.Wrap(err, "closing tempfile")
+	}
+	defer os.Remove(tmpfile.Name())
+
 	return Load(ctx, pkgdir, func(dir string) error {
 		prog := filepath.Join(dir, "x")
-		args = append([]string{"-d", cwd}, args...)
+		args = append([]string{cwd, tmpfile.Name()}, args...)
 		cmd := exec.CommandContext(ctx, prog, args...)
 		cmd.Dir = dir
 		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 		fmt.Printf("xxx about to run %#v\n", cmd)
-		return cmd.Run()
+		err := cmd.Run()
+		if err != nil {
+			return errors.Wrap(err, "running subprocess")
+		}
+		f, err := os.Open(tmpfile.Name())
+		if err != nil {
+			return errors.Wrapf(err, "opening tempfile")
+		}
+		defer f.Close()
+		dec := json.NewDecoder(f)
+		var errstrs []string
+		err = dec.Decode(&errstrs)
+		if err != nil {
+			return errors.Wrap(err, "parsing subprocess output")
+		}
+
+		switch len(errstrs) {
+		case 0:
+			return nil
+		case 1:
+			return errors.New(errstrs[0])
+		default:
+			errs := make([]error, 0, len(errstrs))
+			for _, e := range errstrs {
+				errs = append(errs, errors.New(e))
+			}
+			return multierr.Combine(errs...)
+		}
 	})
 }
