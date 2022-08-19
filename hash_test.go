b2c36a3ade6e91a59ec9f7b338ceff024877c43d
Bob Glickstein <bobg@emphatic.com>
2022-08-18 18:29:50 -0700

    Add more test cases for HashTarget.

--- hash_test.go
+++ hash_test.go
@@ -2,6 +2,7 @@ package fab
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"io/fs"
 	"os"
@@ -22,7 +23,9 @@ func TestHashTarget(t *testing.T) {
 	inPath := filepath.Join(dir, "in")
 	outPath := filepath.Join(dir, "out")
 
-	err = os.WriteFile(inPath, []byte("foo"), 0644)
+	teststr := "foo"
+
+	err = os.WriteFile(inPath, []byte(teststr), 0644)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -56,10 +59,10 @@ func TestHashTarget(t *testing.T) {
 				got = nil
 			}
 			if want {
-				if string(got) != expect+"foo" {
-					t.Errorf("got %s, want %sfoo", string(got), expect)
+				if string(got) != expect+teststr {
+					t.Errorf("got %s, want %s%s", string(got), expect, teststr)
 				} else {
-					expect += "foo"
+					expect += teststr
 				}
 			} else {
 				if string(got) != expect {
@@ -76,6 +79,56 @@ func TestHashTarget(t *testing.T) {
 
 	t.Run("second run, with empty db", try(true))
 	t.Run("third run, with non-empty db", try(false))
+
+	// Rewrite inPath but don't change its contents.
+	err = os.WriteFile(inPath, []byte(teststr), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("fourth run, with unaltered inPath but new modtime", try(false))
+
+	// Rewrite inPath and do change its contents.
+	teststr = "bar"
+	err = os.WriteFile(inPath, []byte(teststr), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("fifth run, with altered inPath", try(true))
+
+	// Remove outPath.
+	err = os.Remove(outPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expect = ""
+
+	t.Run("sixth run, with removed outPath", try(true))
+
+	// Alter outPath.
+	expect = teststr + "x"
+	err = os.WriteFile(outPath, []byte(expect), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("seventh run, with altered outPath", try(true))
+
+	// Set inPath and outPath to an earlier state that's in the db.
+	expect = "foofoo"
+	teststr = "foo"
+	err = os.WriteFile(inPath, []byte(teststr), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.WriteFile(outPath, []byte(expect), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("eighth run, with an earlier state restored", try(false))
 }
 
 type memdb set.Of[string]
@@ -83,10 +136,10 @@ type memdb set.Of[string]
 var _ HashDB = memdb{}
 
 func (m memdb) Has(_ context.Context, h []byte) (bool, error) {
-	return (set.Of[string])(m).Has(string(h)), nil
+	return (set.Of[string])(m).Has(hex.EncodeToString(h)), nil
 }
 
 func (m memdb) Add(_ context.Context, h []byte) error {
-	(set.Of[string])(m).Add(string(h))
+	(set.Of[string])(m).Add(hex.EncodeToString(h))
 	return nil
 }
