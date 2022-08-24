fbe321670882ba8d06de26650f8d6fa597b62dd0
Bob Glickstein <bobg@emphatic.com>
2022-08-24 15:08:25 -0700

    Add BenchmarkCompile.

--- compile_test.go
+++ compile_test.go
@@ -13,91 +13,115 @@ import (
 )
 
 func TestCompile(t *testing.T) {
+	tbCompile(t)
+}
+
+func BenchmarkCompile(b *testing.B) {
+	tbCompile(b)
+}
+
+// Test or benchmark the compiler.
+func tbCompile(tb testing.TB) {
 	tmpdir, err := os.MkdirTemp("", "fab")
 	if err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 	defer os.RemoveAll(tmpdir)
 
 	ctx := context.Background()
 
 	if err = copy.Copy("_testdata/compile", tmpdir); err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 
 	cwd, err := os.Getwd()
 	if err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 	if err = os.Chdir(tmpdir); err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 	defer os.Chdir(cwd)
 
 	tmpfile, err := os.CreateTemp("", "fab")
 	if err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 	tmpname := tmpfile.Name()
 	defer os.Remove(tmpname)
 	if err = tmpfile.Close(); err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 
-	var (
-		pkgdir  = filepath.Join(tmpdir, "pkg")
-		rulesgo = filepath.Join(pkgdir, "rules.go")
-	)
+	pkgdir := filepath.Join(tmpdir, "pkg")
 
-	if err = Compile(ctx, pkgdir, tmpname); err != nil {
-		t.Fatal(err)
+	if b, ok := tb.(*testing.B); ok {
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			if err = Compile(ctx, pkgdir, tmpname); err != nil {
+				b.Fatal(err)
+			}
+		}
+
+		return
 	}
 
-	info, err := os.Stat(tmpname)
-	if err != nil {
-		t.Fatal(err)
-	}
-	modtime := info.ModTime()
+	if t, ok := tb.(*testing.T); ok {
+		rulesgo := filepath.Join(pkgdir, "rules.go")
 
-	time.Sleep(time.Second)
+		if err = Compile(ctx, pkgdir, tmpname); err != nil {
+			t.Fatal(err)
+		}
 
-	cmd := exec.CommandContext(ctx, tmpname, "noop")
-	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
-	if err = cmd.Run(); err != nil {
-		t.Fatal(err)
-	}
+		info, err := os.Stat(tmpname)
+		if err != nil {
+			t.Fatal(err)
+		}
+		modtime := info.ModTime()
 
-	info, err = os.Stat(tmpname)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !modtime.Equal(info.ModTime()) {
-		t.Error("driver got rebuilt unexpectedly")
-	}
+		time.Sleep(time.Second)
 
-	f, err := os.OpenFile(rulesgo, os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-	fmt.Fprintln(f, "// comment")
-	if err = f.Close(); err != nil {
-		t.Fatal(err)
-	}
+		cmd := exec.CommandContext(ctx, tmpname, "noop")
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+		if err = cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
 
-	time.Sleep(time.Second)
+		info, err = os.Stat(tmpname)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !modtime.Equal(info.ModTime()) {
+			t.Error("driver got rebuilt unexpectedly")
+		}
 
-	cmd = exec.CommandContext(ctx, tmpname, "noop")
-	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
-	if err = cmd.Run(); err != nil {
-		t.Fatal(err)
-	}
+		f, err := os.OpenFile(rulesgo, os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		fmt.Fprintln(f, "// comment")
+		if err = f.Close(); err != nil {
+			t.Fatal(err)
+		}
 
-	info, err = os.Stat(tmpname)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if modtime.Equal(info.ModTime()) {
-		t.Error("driver did not get rebuilt but should have")
+		time.Sleep(time.Second)
+
+		cmd = exec.CommandContext(ctx, tmpname, "noop")
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+		if err = cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+
+		info, err = os.Stat(tmpname)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if modtime.Equal(info.ModTime()) {
+			t.Error("driver did not get rebuilt but should have")
+		}
+
+		return
 	}
 }
