5cf8a20f45c1a2758393d0afb5d3c96d0e650d94
Bob Glickstein <bobg@emphatic.com>
2022-08-30 17:23:50 -0700

    Add test coverage.

--- _testdata/compile/pkg/rules.go
+++ _testdata/compile/pkg/rules.go
@@ -3,3 +3,7 @@ package rules
 import "github.com/bobg/fab"
 
 var Noop = &fab.Command{Shell: "sh -c 'echo hello'"}
+
+var notExported = &fab.Command{Shell: "sh -c 'echo not exported'"}
+
+var NotATarget = 17
